72e5678c97ecdfdae843644f987677dfcb93cbba
zhengwenhao <931516643@qq.com>
2022-07-08 09:16:17 +0800

    feat(husky+eslint配置等): 配置选项

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import './styles/index.scss'
+const app = createApp(App)
+app.use(store).use(router).use(ElementPlus).mount('#app')
